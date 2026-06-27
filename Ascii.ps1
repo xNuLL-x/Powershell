7eb357fe019a2c68688d3f55f62bfbe3cf05bc65
Micky <Mickyddroy@Gmail.com>
2026-06-27 10:18:20 -0400

    Delete Ascii.ps1

--- Ascii.ps1
+++
@@ -1,5 +0,0 @@
-$CmdExec="dir C:\Users\"
-$AscIIBytes=[System.Text.Encoding]::ASCII.GetBytes($CmdExec)
-Write-Host "ASCII Bytes: $($asciiBytes -join ', ')"
-$decodedCommand = [System.Text.Encoding]::ASCII.GetString($ascIIBytes)
-Invoke-Expression $decodedCommand
