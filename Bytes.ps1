4ece027838bbda8228c35ecf81e71641e0c35141
Micky <Mickyddroy@Gmail.com>
2026-06-27 10:18:37 -0400

    Delete Bytes.ps1

--- Bytes.ps1
+++
@@ -1,5 +0,0 @@
-$bytes=[byte[]](72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100)
-$outputFile = "C:\Users\0000\Documents\Bytes.bin"
-[System.IO.File]::WriteAllBytes($outputFile, $bytes)
-
-Write-Host "[+] $outputFile"
