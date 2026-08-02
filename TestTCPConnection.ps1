faddb069c6ad213910ded92414066afec9eca694
Micky <Mickyddroy@Gmail.com>
2026-08-02 11:53:32 -0400

    Delete TestTCPConnection.ps1

--- TestTCPConnection.ps1
+++
@@ -1,5 +0,0 @@
-$Target="Test-NetConnection -CommonTCPPort HTTP " <#Test connection ++ ASCII Encoding#>
-$Bytes=[System.Text.Encoding]::ASCII.GetBytes($Target)
-Write-Host "ASCII: $($Bytes -join ', ')"
-$DecodeCommand=[System.Text.Encoding]::ASCII.GetString($Bytes)
-Invoke-Expression $DecodeCommand
